49fa39dc50d18b1bce8ce8df8db1505384d3dfaf
Hugo Marcotte <marcotte.hugo@gmail.com>
2016-11-24 10:29:43 -0500

    Show user fB profile pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.image = auth.info.image
       user.save!
     end
   end

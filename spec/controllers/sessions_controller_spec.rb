e1d8f1bcf2a596761e91ef31f38f5aa2f20e40c2
Hugo Marcotte <marcotte.hugo@gmail.com>
2016-12-04 21:01:33 -0500

    Make all test pass

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,19 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-
   describe "GET #create" do
+    let(:user) { instance_double(User, id: 123) }
+
     it "returns http success" do
+      allow(User).to receive(:from_omniauth).and_return(user)
+
       get :create
-      expect(response).to have_http_status(:success)
+
+      expect(response).to have_http_status(302)
     end
   end
 
   describe "GET #destroy" do
     it "returns http success" do
       get :destroy
-      expect(response).to have_http_status(:success)
+
+      expect(response).to have_http_status(302)
     end
   end
-
 end
